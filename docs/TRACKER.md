7765794d5974e29d1c18be09313348cc49c0b3b7
Maxwell Griffith <maxwellgriffith345@gmail.com>
2026-01-28 19:35:32 -0600

    clean up data pull

---
+++ docs/TRACKER.md
@@ -0,0 +1,10 @@
+#TODO LIST
+
+- 1/28/26
+  - working on writing pull_data script
+  - ran into issue with gridstatus get_lmp being depricated
+  - TODO
+  - refactor with new method
+  - catch erros when there is no data
+  - process a year in chunks so it doesn't take foreever
+  - clean up the rest of the pull data process 
--- src/get_data.py
+++ src/get_data.py
@@ -1,1 +1,35 @@
-# place holder to write script to pull data
+"""
+Uses gridstatus API to pull SPP nodal prices
+"""
+
+import gridstatus
+import pandas as pd
+
+
+locations = [
+"WR.LEC.5",
+"WR.JEC.1",
+"KCPLIATANUNIAT1",
+"KCPLLACYGNEUNLAC1",
+"MPS.ROCKCREEK",
+"SECI_CIMARRON",
+"SECI.KCPS.SPEARVILLE"]
+
+iso = gridstatus.SPP()
+
+df_main=iso.get_lmp(date="2023-01-01",
+    end = "2024-01-01", market="DAY_AHEAD_HOURLY")
+
+df_main=df_main.loc[df_main['Location'].isin(locations)]
+
+df_main.reset_index(drop=True)
+
+
+
+
+
+df_main.drop(columns=["Interval Start", "Interval End",
+    "Market", "Location Type", "PNode", "Energy",
+    "Congestion", "Loss"], inplace=True)
+
+df_main.reset_index(drop=True)

af061c39fd1f9379dbaa23f3e0142093cb146f85
Maxwell Griffith <maxwellgriffith345@gmail.com>
2026-01-29 18:51:26 -0600

    still working on data pull

--- src/get_data.py
+++ src/get_data.py
@@ -4,7 +4,22 @@ Uses gridstatus API to pull SPP nodal prices
 
 import gridstatus
 import pandas as pd
+import os
 
+
+""" Set Paths For Exporting Data """
+#how much path checking to do?
+#Get path to current dir /src
+current_dir = os.getcwd()
+
+#get path to main project path
+main_dir = os.path.dirname(current_dir)
+
+#create path to data folder
+data_path = os.path.join(current_dir, "data")
+
+
+""" Pull Data From Grid Status """
 
 locations = [
 "WR.LEC.5",
@@ -17,8 +32,12 @@ locations = [
 
 iso = gridstatus.SPP()
 
-df_main=iso.get_lmp(date="2023-01-01",
-    end = "2024-01-01", market="DAY_AHEAD_HOURLY")
+# Wrap in a try except? sometimes years aren't available
+# don't wan tht escript to crash
+
+df_main = iso.get_lmp_day_ahead_hourly(date="2024-01-01", end = "2024-01-02",
+    location_type =  "Hub")
+
 
 df_main=df_main.loc[df_main['Location'].isin(locations)]
 
